03e5152fc43c396c974a72a57f04cf3bde6abfa7
sapnakushwah011 <sapnakushwah072@gmail.com>
2023-12-12 11:59:03 +0530

    first commit

---
+++ tribute project/script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('scrollButton').addEventListener('click', function() {
+    // Scroll down 500 pixels (adjust as needed)
+    window.scrollBy({
+        top: 500,
+        behavior: 'smooth' // Use smooth scrolling
+    });
+});
